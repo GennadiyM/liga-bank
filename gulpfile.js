1a625c72225326d44ca5c4d484dbad8ed300b056
Геннадий Максимов <mr.gennadiy.maksimov@gmail.com>
2020-06-10 16:11:12 +0300

    fonts, new image, favicon

--- gulpfile.js
+++ gulpfile.js
@@ -110,7 +110,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source//*.ico"
+    "source/favicon/**"
     ], {
       base: "source"
     })
