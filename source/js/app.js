8f7114347c87a48a25412c798b10c1c35e059be6
Геннадий Максимов <mr.gennadiy.maksimov@gmail.com>
2020-06-26 10:36:14 +0300

    refresh calc

--- source/js/app.js
+++ source/js/app.js
@@ -1,22 +1,22 @@
-import 'element-closest-polyfill';
-import 'core-js/features/dom-collections/for-each';
+// import 'element-closest-polyfill';
+// import 'core-js/features/dom-collections/for-each';
 import promo from './components/promo.js';
 import select from './components/select.js';
 import services from './components/services.js';
-import popupLogin from './components/popup-login.js';
-import contacts from './components/contacts.js';
-import mobMenu from './components/menu.js';
+// import popupLogin from './components/popup-login.js';
+// import contacts from './components/contacts.js';
+// import mobMenu from './components/menu.js';
 import utils from './components/utils.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   utils();
   select();
-  popupLogin();
+  // popupLogin();
 });
 
 window.onload = function () {
   promo();
   services();
-  contacts();
-  mobMenu();
+  // contacts();
+  // mobMenu();
 };
